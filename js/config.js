13d8871fb72f5ecee1121fe1f783b0a2d050564b
MaiNorapong <MaiNorapong@users.noreply.github.com>
2020-08-14 16:21:32 +0700

    Update website config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_z7hbr9gA7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3tmq5diufdj1ba56tijqa2al2k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_SgVfFoafT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '743njucvfe2mtjg1j8b4789v2o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://kqxq7t4w3j.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://1ytr14jbd8.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
