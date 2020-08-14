9a2a439a101b93f75d91a565208e5a653983ddea
MaiNorapong <MaiNorapong@users.noreply.github.com>
2020-08-14 18:02:18 +0700

    Update website config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_SgVfFoafT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '743njucvfe2mtjg1j8b4789v2o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_tQnhjiyLd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2u4in5ht8artr3pdoae472or2l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://ln19n9lu03.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://q5t0ih42da.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
